ac8078e788041dea5eba5e71e3add7c6c38e432a
slepaget <simonlepagetrudeau@hotmail.com>
2021-05-20 18:58:59 -0400

    test1

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonjournom;
+import aleatoire.*;
+/**
+ *
+ * @author Simon
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Person john = new Person(args[0], args[1], Integer.parseInt(args[2]));
+        
+        System.out.println("Prenom :" + john.getFirstName());
+        
+        System.out.println("Nom? :" + john.getLastName());
+        
+        System.out.println("Bonjour John Doe");
+        
+    }
+    
+}
